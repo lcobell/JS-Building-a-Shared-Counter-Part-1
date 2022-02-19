0da6fcdd5597e6fad71a3562ed6cfbfec42a1393
lcobell <lcobell@gmail.com>
2022-02-19 12:40:29 -0600

    Still working on Bonus
    Reset Button needs work

--- public/index.js
+++ public/index.js
@@ -2,8 +2,26 @@ function main(){
     const countContainer = document.querySelector('#count-container');
     const incrementButton = document.querySelector('#increment-button');
     const decrementButton = document.querySelector('#decrement-button');
-
     let countValue = 0;
+
+
+
+    async function getStartCount() {
+
+  
+        let newCount = await fetch ('http://localhost:9001/counter')
+        let countResponse = await newCount.json()
+        // console.log(countResponse)
+        return countResponse
+        
+    }
+
+    getStartCount().then(function(resp) {
+        console.log(resp.value)
+        countValue = resp.value;
+        countContainer.textContent = countValue;
+    })
+  
 
     function increment(){
         countValue++;
@@ -14,7 +32,11 @@ function main(){
         countValue--;
         countContainer.textContent = countValue;
     }
-
+    // function reset() {
+    //     countValue = 0
+    //     countContainer.textContent = countValue;
+    // }
+    // resetButton.addEventListener('click', reset);
     incrementButton.addEventListener('click', increment);
     decrementButton.addEventListener('click', decrement);
     countContainer.textContent = countValue;
